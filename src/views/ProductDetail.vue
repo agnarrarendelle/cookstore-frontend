996336abf02ced8854acc5e41e1ad87b78354c8c
agnarrarendelle <danny20001102@gmail.com>
2023-11-18 17:12:50 +0800

    added componenets for about, product and product details page

---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+
+const productId = route.params.productId
+
+</script>
+<template>
+    <h1>route id is {{ productId }}</h1>
+</template>
+
+<style scoped></style>
