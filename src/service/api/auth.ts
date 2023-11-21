5adfc03b0cdabc99bff88379dfa6a74162f5dd48
agnarrarendelle <danny20001102@gmail.com>
2023-11-22 01:56:41 +0800

    added some wrapper methods for api calls to authentication stuffs

---
+++ src/service/api/auth.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from "axios";
+import { ApiResponse, UserEntryResult } from "../response-type";
+export const login = async (
+  email: string,
+  password: string
+): Promise<ApiResponse<UserEntryResult>> => {
+  const res = await axios.post<ApiResponse<UserEntryResult>>("/admin/login", {
+    email,
+    password,
+  });
+  return res.data;
+};
+
+export const refresh = async (): Promise<ApiResponse<UserEntryResult>> => {
+  const res = await axios.post<ApiResponse<UserEntryResult>>("/admin/refresh");
+  return res.data;
+};
+
+export const validate = async ():Promise<AxiosResponse<ApiResponse<UserEntryResult>, any>> => {
+  const res = await axios.post<ApiResponse<UserEntryResult>>("/admin/validate");
+  return res;
+};
