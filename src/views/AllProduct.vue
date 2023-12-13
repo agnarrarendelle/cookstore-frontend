997b8369c71adfdfe59583aed02c02593e927979
agnarrarendelle <danny20001102@gmail.com>
2023-12-13 16:49:23 +0800

    added a page to display all products

--- src/views/AllProduct.vue
+++ src/views/AllProduct.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { CategoryResult } from "../service/response-type"
+import { getCategories } from "../service/api/category"
+import { reactive, onMounted } from "vue"
+import CategoryProductList from "../components/CategoryProductList.vue";
+onMounted(async () => {
+    const res = await getCategories()
+    categories.push(...res.data)
+})
+
+const categories = reactive<CategoryResult[]>([])
 
 
 </script>
 <template>
     <h1>This is product page</h1>
+    <template v-for="category in categories">
+        <CategoryProductList :category-id="category.id!"></CategoryProductList>
+    </template>
 </template>
 
 <style scoped></style>
