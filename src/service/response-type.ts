d70ea8348832002eefbc8afa66d664bbab8190b1
agnarrarendelle <danny20001102@gmail.com>
2023-12-13 16:46:56 +0800

    added more response data types

--- src/service/response-type.ts
+++ src/service/response-type.ts
@@ -10,5 +10,19 @@ export interface UserEntryResult {
 
 export interface ProductImageResult {
   uploadUrl: string;
-  imageId: string
+  imageId: string;
+}
+
+export interface ProductResult {
+  name: string;
+  status: string;
+  description: string;
+  price: number;
+  discount: number;
+  imageUrl: string;
+}
+export interface CategoryResult {
+  name: string;
+  id: number | null;
+  products: Array<ProductResult>;
 }
