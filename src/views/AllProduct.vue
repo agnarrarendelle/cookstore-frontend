06466c84dfecf66327b9d86d98e8a79e0b39df96
agnarrarendelle <danny20001102@gmail.com>
2023-12-14 14:24:01 +0800

    placed the new component in the products page

--- src/views/AllProduct.vue
+++ src/views/AllProduct.vue
@@ -3,19 +3,45 @@ import { CategoryResult } from "../service/response-type"
 import { getCategories } from "../service/api/category"
 import { reactive, onMounted } from "vue"
 import CategoryProductList from "../components/CategoryProductList.vue";
+
+interface OrderItem {
+    name: string,
+    price: number,
+    number: number
+}
 onMounted(async () => {
     const res = await getCategories()
     categories.push(...res.data)
 })
 
 const categories = reactive<CategoryResult[]>([])
+const orderItems = reactive<Map<number, OrderItem>>(new Map())
+
+const addOrderItem = (id: number, name: string, price: number, number: number) => {
+    if (orderItems.has(id)) {
+        orderItems.get(id)!.number += number
+        return
+    }
+
+    const newOrderItem: OrderItem = { name, price, number }
+    orderItems.set(id, newOrderItem)
+}
 
 
+const removeOrderItem = (id: number) => {
+    orderItems.delete(id)
+}
 </script>
 <template>
-    <h1>This is product page</h1>
+    <div class="w-11/12 my-2 text-end">
+        <button class="right-0 top-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Submit your order
+        </button>
+    </div>
+
     <template v-for="category in categories">
-        <CategoryProductList :category-id="category.id!"></CategoryProductList>
+        <CategoryProductList :category-id="category.id!" :add-order-item="addOrderItem"
+            :remove-order-item="removeOrderItem"></CategoryProductList>
     </template>
 </template>
 
