8000856b84fb6428d079f90a1a0251c0dd90b171
agnarrarendelle <danny20001102@gmail.com>
2023-12-20 22:59:04 +0800

    Init websocket client and show unpaid orders

--- src/views/admin/AdminIndex.vue
+++ src/views/admin/AdminIndex.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
+import { getUnpaidOrders } from "../../service/api/admin/orders";
+import { OrderResult } from "../../service/response-type";
+import { useStore } from "../../state"
+import { onMounted, reactive } from "vue"
+import { MutationTypes } from "../../state/mutation-types";
+import OrderListVue from "../../components/OrderList.vue";
+const store = useStore()
 
+const unpaidProducts = reactive<OrderResult[]>([])
+
+onMounted(async () => {
+    const res = await getUnpaidOrders()
+    unpaidProducts.push(...res.data)
+    store.commit(MutationTypes.INIT_WEB_SOCKET, { serverUrl: '', subscribeUrl: '', orders: unpaidProducts })
+})
 
 </script>
 <template>
-    <h1>This is admin index page</h1>
-    <router-view></router-view>
+    <OrderListVue :orders="unpaidProducts"></OrderListVue>
 </template>
 
 <style scoped></style>
