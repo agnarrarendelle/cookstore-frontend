4fba320b82a920b2da6b312ee64b3c377cb3304d
agnarrarendelle <danny20001102@gmail.com>
2023-12-20 22:55:18 +0800

    changed mutation method argument type

--- src/state/mutations.ts
+++ src/state/mutations.ts
@@ -3,16 +3,22 @@ import { MutationTypes } from "./mutation-types";
 import { MutationTree } from "vuex";
 import { Client } from "@stomp/stompjs";
 import { OrderResult } from "../service/response-type";
+type WebsocketConfig = {
+  serverUrl:string
+  subscribeUrl:string,
+  orders:OrderResult[]
+}
+
 export type Mutations<S = State> = {
   [MutationTypes.SET_JWT_AUTH_TOKEN](state: S, newToken: string | null): void;
-  [MutationTypes.INIT_WEB_SOCKET](state: S, url: string): void;
+  [MutationTypes.INIT_WEB_SOCKET](state: S, config:WebsocketConfig): void;
 };
 
 export const mutations: MutationTree<State> & Mutations = {
   [MutationTypes.SET_JWT_AUTH_TOKEN](state, newToken: string | null) {
     state.jwtAuthToken = newToken;
   },
-  [MutationTypes.INIT_WEB_SOCKET](state, url: string): void {
+  [MutationTypes.INIT_WEB_SOCKET](state, config:WebsocketConfig): void {
     const client = new Client({
       brokerURL: "ws://localhost:8080/ws-endpoint",
       connectHeaders: {
@@ -20,8 +26,9 @@ export const mutations: MutationTree<State> & Mutations = {
       },
       onConnect: () => {
         client.subscribe("/user/" + "matt" + "/new_order", (message) => {
+          console.log("new order!")
           const newOrder: OrderResult = JSON.parse(message.body);
-          state.orders.push(newOrder);
+          config.orders.push(newOrder);
         });
       },
       onWebSocketError: (e) => {
