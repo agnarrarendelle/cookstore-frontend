0fc2ff1d0231b3a650e8cc310d09b60acb825875
agnarrarendelle <danny20001102@gmail.com>
2023-12-20 12:11:28 +0800

    added getters for the two states added

--- src/state/getter.ts
+++ src/state/getter.ts
@@ -1,12 +1,16 @@
 import { GetterTree } from "vuex";
 import { State } from "./state";
+import { Client } from "@stomp/stompjs";
+import { OrderResult } from "../service/response-type";
 
 export type Getters = {
   jwtAuthToken(state: State): string | null;
+  websocket(state: State): Client | null;
+  orders(state: State): OrderResult[];
 };
 
 export const getters: GetterTree<State, State> & Getters = {
-  jwtAuthToken: (state) => {
-    return state.jwtAuthToken;
-  },
+  jwtAuthToken: (state) => state.jwtAuthToken,
+  websocket: (state) => state.websocket,
+  orders: (state) => state.orders,
 };
