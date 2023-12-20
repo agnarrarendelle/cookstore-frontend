1dd6b8f150de4676e9befba825e036c6898b1774
agnarrarendelle <danny20001102@gmail.com>
2023-12-20 12:11:08 +0800

    added websocket client and response data to vuex state

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,7 +1,13 @@
+import { Client } from "@stomp/stompjs";
+import { OrderResult } from "../service/response-type";
 export type State = {
   jwtAuthToken: string | null;
+  websocket: Client | null;
+  orders: OrderResult[];
 };
 
 export const state: State = {
   jwtAuthToken: "",
+  websocket: null,
+  orders: [],
 };
