f99a8e9804d9e9d92978716a8b1b6849ca5462bc
agnarrarendelle <danny20001102@gmail.com>
2023-12-21 18:25:06 +0800

    added mutation types

--- src/state/mutation-types.ts
+++ src/state/mutation-types.ts
@@ -2,5 +2,8 @@ export enum MutationTypes {
   SET_JWT_AUTH_TOKEN = "setJwtAuthToken",
   REMOVE_JWT_AUTH_TOKEN = "removeJwtAuthToken",
 
-  INIT_WEB_SOCKET = "setWebSocket",
+  INIT_WEB_SOCKET = "initWebSocket",
+  SET_UNPAID_ORDERS = "setUnpaidOrders",
+  MARK_ORDER_PAID = "markOrderPaid",
+  CANCEL_ORDER = "cancelOrder",
 }
