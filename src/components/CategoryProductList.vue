24769b86f838c015156d643e043b706cf892d247
agnarrarendelle <danny20001102@gmail.com>
2023-12-21 18:36:30 +0800

    removed unused css

--- src/components/CategoryProductList.vue
+++ src/components/CategoryProductList.vue
@@ -107,26 +107,4 @@ const closeModal = () => {
 </template>
 
 <style scoped>
-.time {
-    font-size: 12px;
-    color: #999;
-}
-
-.bottom {
-    margin-top: 13px;
-    line-height: 12px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-
-.button {
-    padding: 0;
-    min-height: auto;
-}
-
-.image {
-    width: 100%;
-    display: block;
-}
 </style>
