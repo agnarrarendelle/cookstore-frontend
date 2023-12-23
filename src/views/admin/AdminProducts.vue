e506ed1ef01867e8498985ca69f8d4fa0fa2ba4c
agnarrarendelle <danny20001102@gmail.com>
2023-12-23 17:21:35 +0800

    added a page to display all products for admin

--- src/views/admin/AdminProducts.vue
+++ src/views/admin/AdminProducts.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-
-
+import CategoryProductListAdmin from '../../components/CategoryProductListAdmin.vue';
 </script>
 <template>
     <h1>This is admin product page</h1>
-
+    <CategoryProductListAdmin></CategoryProductListAdmin>
 </template>
 
 <style scoped></style>
