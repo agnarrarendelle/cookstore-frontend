5439b13af6821c0107df8d93dbe430ccee419fea
agnarrarendelle <danny20001102@gmail.com>
2023-12-24 14:36:51 +0800

    added a method to add a new category

--- src/service/api/category.ts
+++ src/service/api/category.ts
@@ -16,4 +16,8 @@ export const getCategoryWithProduct = async (id: number) => {
 export const getCategoriesWithProducts = async()=>{
   const res = await axios.get<ApiResponse<Array<CategoryResult>>>("/category/products");
   return res.data;
-}
+};
+
+export const addCategory = async (body: CategoryBody) => {
+  await axios.post<ApiResponse<void>>("admin/category/add", body);
+};
